0e2479078d2484d7be2f9d0788e4dedd00063926
ratushniak.denis <ratushniak.denis@gmail.com>
2022-10-09 14:47:39 +0300

    Added login function skeleton

---
+++ backendapi/api/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from rest_framework import viewsets
+from django.contrib.auth.models import User
+
+
+# from .serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    # serializer_class = UserSerializer
+
+
+def login_(request):
+    # creating fake users
+    registered_users = {'beb@gmail.com': 'bebbebbeb',
+                         'bebbeb@gmail.com': 'beeeeeeb',
+                         'bebebebey@gmail.com': 'bebebebey'}
+    for (email, password) in registered_users:
+        my_user = User.objects.create_user(username=email, email=email, password=password)
+        my_user.is_active = False
+        my_user.save()
+
+    if request.method == 'POST':
+        username = request.POST['email']
+        password1 = request.POST['password']
+
+        user = authenticate(username=username, password=password1)
+
+        if user is not None:
+            login(request, user)
+            # messages.success(request, "Logged In Sucessfully!!")
+            return redirect('index.html')
+        else:
+            messages.error(request, "Bad Credentials!!")
+            return render(request, 'samepage.html')
+
+    return render(request, 'samepage.html')
