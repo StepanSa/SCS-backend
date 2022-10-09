9c095d4704597fe4f9b6d9ac48da5d4bd4143455
Stepan Sarabun <stepankolv12@gmail.com>
2022-10-09 14:50:58 +0300

    Added serializers

--- backendapi/api/views.py
+++ backendapi/api/views.py
@@ -1,8 +1,14 @@
-from rest_framework import viewsets
-from django.contrib.auth.models import User
-# from .serializers import UserSerializer
+# from rest_framework import viewsets
+# from django.contrib.auth.models import User
+
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
+
+from .models import User, Sport, Location
+from .serializers import UserSerializer, SportSerializer, LocationSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    # serializer_class = UserSerializer
+
+
