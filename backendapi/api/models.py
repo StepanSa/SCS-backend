97023719dd6f847b32e80e45dea05ebb7de6950d
Stepan Sarabun <stepankolv12@gmail.com>
2022-10-14 20:50:20 +0300

    Updated database
    Added username field to User. Set foreign key in Location to sportId

--- backendapi/api/models.py
+++ backendapi/api/models.py
@@ -5,7 +5,8 @@ class User(models.Model):
     userId = models.AutoField(primary_key=True)
     firstName = models.CharField(max_length=30)
     lastName = models.CharField(max_length=30)
-    email = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
     password = models.CharField(max_length=30)
     birthDate = models.DateField(blank=True, null=True)
 
@@ -17,8 +18,7 @@ class Sport(models.Model):
 
 class Location(models.Model):
     locationId = models.AutoField(primary_key=True)
-    sportId = models.ForeignKey('Sport', on_delete=models.CASCADE)
+    sportId = models.ForeignKey('Sport', to_field='sportId', on_delete=models.CASCADE)
     address = models.CharField(max_length=150)
     tgChannel = models.CharField(max_length=50)
-
 
