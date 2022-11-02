6c7274165ca3aa1d9bcae2b9f53920b7dab145ec
ratushniak.denis <ratushniak.denis@gmail.com>
2022-11-02 15:34:45 +0200

    Added feature to get photo from C:/images/

--- backendapi/backendapi/settings.py
+++ backendapi/backendapi/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 import ssl
 
@@ -138,6 +138,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = (
+    os.path.join("C:/images"),
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- backendapi/backendapi/urls.py
+++ backendapi/backendapi/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('', include('api.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
